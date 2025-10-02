2e8c08a51af56a901476757c0bf8b995919a8c9d
nadim-chowdhury <nadim-chowdhury@outlook.com>
2025-10-03 00:57:57 +0600

    learned lists

--- lib/test.dart
+++ lib/test.dart
@@ -9,17 +9,33 @@ void main() {
 
   // stdout.write('Hola $name');
 
-  var nadim = new Human();
+  // var nadim = new Human();
 
-  String name = 'Lieo';
+  // String name = 'Lieo';
 
-  int val = 906;
+  // int val = 906;
 
-  BigInt value = BigInt.parse('90000000000000');
+  // BigInt value = BigInt.parse('90000000000000');
+
+  var myclass = myClass();
+
+  myclass.printName('Nadim');
+
+  var list1 = [1, 2, 3, 4, 5, 6, 7];
+
+  list1.add(8);
+
+  list1.insert(2, 88);
 }
 
 class Human {
   // Human() {
   //   stdout.write('Hello');
   // }
+}
+
+class myClass {
+  void printName(String name) {
+    print('Hello World! $name');
+  }
 }
