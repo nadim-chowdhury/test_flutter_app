63bcb5a610b152871e41312828d33dd3ce9d30d5
nadim-chowdhury <nadim-chowdhury@outlook.com>
2025-10-02 20:20:48 +0600

    learning dart

---
+++ lib/test.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+void main() {
+  // print('Hello World!');
+
+  stdout.write('Hola');
+
+  // var name = stdin.readLineSync();
+
+  // stdout.write('Hola $name');
+
+  var nadim = new Human();
+
+  String name = 'Lieo';
+
+  int val = 906;
+
+  BigInt value = BigInt.parse('90000000000000');
+}
+
+class Human {
+  // Human() {
+  //   stdout.write('Hello');
+  // }
+}
